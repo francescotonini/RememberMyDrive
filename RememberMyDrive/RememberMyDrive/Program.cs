c26a7e67bfe1a5bf8672d006e2bd5f6b7fd6fb68
Francesco Tonini <francescoantoniotonini@gmail.com>
2015-03-17 21:21:54 +0100

    First release
    First release... Yay :)

---
+++ RememberMyDrive/RememberMyDrive/Program.cs
@@ -0,0 +1,146 @@
+﻿/*
+ This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace RememberMyKey
+{
+    class Program
+    {
+        private static Thread trayIconThread; //In order to work properly, place the tray icon on a separate thread.
+
+        //Adding some stuff for hide the Console window
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private static Int32 showWindow = 0;
+
+        private static void Main()
+        {
+            //Create an event which will be fired when user shutdown or logoff/console closing
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+
+            //Start tray icon thread
+            trayIconThread = new Thread(AddToSystemTray);
+            trayIconThread.Start();
+
+            //Hide the Console window. It can be opened via the Tray Icon
+            ShowWindow(ThisConsole, showWindow);
+        }
+
+        static void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            if (USBDevice.GetAllRemovableDevices().Length != 0)
+            {
+                MessageBox.Show("Ehi! You forgot your drives on the PC.", "REMEMBER YOUR DRIVE!!!!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        /// <summary>
+        /// Add the program to the system tray
+        /// </summary>
+        private static void AddToSystemTray()
+        {
+            NotifyIcon trayIcon = new NotifyIcon();
+            
+            //Assign icon file to tray icon + contextmenustrip + name
+            trayIcon.Text = "Remember my drive";
+            trayIcon.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+
+            //Info button
+            trayIcon.ContextMenuStrip.Items.Add(new ToolStripMenuItem());
+            trayIcon.ContextMenuStrip.Items[0].Text = "Info";
+            trayIcon.ContextMenuStrip.Items[0].Click += TrayIconInfoButton_Click;
+
+            //Exit button
+            trayIcon.ContextMenuStrip.Items.Add(new ToolStripMenuItem());
+            trayIcon.ContextMenuStrip.Items[1].Text = "Exit";
+            trayIcon.ContextMenuStrip.Items[1].Click += TrayIconExitButton_Click;
+
+            //Add event click
+            trayIcon.MouseClick += trayIcon_MouseClick;
+
+            //Make it visible
+            trayIcon.Visible = true;
+            Application.Run();
+        }
+
+        /// <summary>
+        /// Info button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void TrayIconInfoButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Remember my drive! Brought to you by Francesco Tonini :)", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Exit button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void TrayIconExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// Mouse click on tray 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void trayIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) 
+            {
+                USBDevice[] devices = USBDevice.GetAllRemovableDevices();
+                string toShow = string.Format("You have {0} removable drives connected", devices.Length);
+                foreach (USBDevice device in devices)
+                {
+                    toShow += string.Format("\n{0} -> {1}", device.DriveLabel, device.Name);
+                }
+                MessageBox.Show(toShow, "Remember my drive", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ RememberMyDrive/RememberMyDrive/USBDevice.cs
@@ -0,0 +1,102 @@
+﻿/*
+ This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RememberMyKey
+{
+    /// <summary>
+    /// Rappresent a single USB Device
+    /// </summary>
+    public class USBDevice
+    {
+        private DriveInfo drive;
+
+        /// <summary>
+        /// Rappresent the label name of the device (ex. "Luca's USB")
+        /// </summary>
+        public string Name
+        {
+            get { return this.drive.VolumeLabel; }
+        }
+
+        /// <summary>
+        /// Indicates the free space available to user
+        /// </summary>
+        public long FreeSpace
+        {
+            get { return this.drive.AvailableFreeSpace; }
+        }
+
+        /// <summary>
+        /// Indicates the total free space available (not the par value)
+        /// </summary>
+        public long TotaleSpace
+        {
+            get { return this.drive.TotalFreeSpace; }
+        }
+
+        /// <summary>
+        /// Get the drive label (ex. "D:")
+        /// </summary>
+        public string DriveLabel
+        {
+            get { return this.drive.Name; }
+        }
+
+        /// <summary>
+        /// Initialize a USBDevice object
+        /// </summary>
+        /// <param name="drive">The actual drive (MUST be a Removable)</param>
+        public USBDevice(DriveInfo drive)
+        {
+            if (drive.DriveType == DriveType.Removable && drive.IsReady) this.drive = drive; //the IsReady property is necessary when suddently a USB devices has been disconnected and the drive is unavailable but still recognized
+        }
+
+        /// <summary>
+        /// Get an array of removable devices
+        /// </summary>
+        /// <returns>An array of USBDevice with ONLY Removable devices</returns>
+        public static USBDevice[] GetAllRemovableDevices()
+        {
+            List<USBDevice> rDevices = new List<USBDevice>();
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    rDevices.Add(new USBDevice(drive));
+                }
+            }
+            return rDevices.ToArray();
+        }
+    }
+}
